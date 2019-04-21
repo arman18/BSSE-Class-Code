e3bc8c140beeb275494e761c45e380a10e36ec8e
Md. Arman Hossain <43715453+arman18@users.noreply.github.com>
2019-04-21 12:14:46 +0600

    Add files via upload

---
+++ Personal-Code/Multiple-cpp-file-compile/file2.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+#include "function.h"
+
+void print_hello()
+{
+	cout<<"hello world hello"<<endl;
+}
---
+++ Personal-Code/Multiple-cpp-file-compile/main.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+#include "function.h"
+using namespace std;
+int main(void)
+{
+	print_hello();
+	cout<<"the factorial of 5 is: "<<factorial(5)<<endl;
+	return 0;
+}
