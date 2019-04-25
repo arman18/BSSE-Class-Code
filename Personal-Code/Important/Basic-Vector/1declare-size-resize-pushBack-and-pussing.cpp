4e88e963c557d5675d139a9ea49eef873ebfa4ee
Md. Arman Hossain <43715453+arman18@users.noreply.github.com>
2019-04-25 18:21:09 +0600

    Update and rename 1declare-size-resize-pushBack.cpp to 1declare-size-resize-pushBack-and-pussing.cpp

--- Personal-Code/Important/Basic-Vector/1declare-size-resize-pushBack-and-pussing.cpp
+++ Personal-Code/Important/Basic-Vector/1declare-size-resize-pushBack-and-pussing.cpp
@@ -2,6 +2,11 @@
 #include<vector>
 using namespace std;
 
+void printElement(vector<int>& v){
+    int siz = v.size();
+    for(int i=0; i<siz; i++) cout<<v[i]<<endl;
+}
+
 int main(void){
 /*How to declare vector and use of size,resize,push_back*/
     vector<int> v(6); //Same as vector<int> v(v,0);
@@ -10,8 +15,8 @@ int main(void){
     cout<<"size: "<<v.size()<<endl;
     v.resize(11,2);
     v.push_back(3); //Push_back always increase the size by 1 and add element to that index
-    for(int i=0; i<11; i++) cout<<v[i]<<endl;
+    printElement(v);
     cout<<"size: "<<v.size()<<endl;
-    
+
     return 0;
 }
