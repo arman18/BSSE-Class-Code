3d3c86998bfe2e47b1553cb9a94222637517a3dd
Md. Arman Hossain <43715453+arman18@users.noreply.github.com>
2019-11-07 00:40:08 +0600

    Add files via upload

---
+++ 4th/sqlite3/schema.sql
@@ -0,0 +1,7 @@
+create table students(name,roll);
+insert into students(name,roll) values('Arman','29');
+insert into students(name,roll) values('Sadikul','03');
+insert into students(name,roll) values('Sami','33');
+insert into students(name,roll) values('Nadim','28');
+insert into students(name,roll) values('Mahdee','10');
+insert into students(name,roll) values('Arif','41');
